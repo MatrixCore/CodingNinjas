def0672f3ba7442755f5533f252a47d559b1782c
MatrixCore <31068187+MatrixCore@users.noreply.github.com>
2017-09-19 16:20:18 +0200

    Add files via upload

---
+++ Law Clinic Skeleton/GUI/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Capture_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            Begins the initial capture of a clients infomation
+
+            Checks will need to be made such as if the ID number provided is valid and matches the date of birth
+            Another important check to consider is if the client is already present in the Clinic's database meaning it is a return visit
+            Consider making these checks as methods rather than coding within the buttonClick event
+
+            Based on the client's answers to the three questions, they will continue on to the means test and the active window should switch
+            the means test's GUI
+            If they answer no to the first three questions then there is no need to continue and complete the means test
+
+            Here is where the client's inital info will be added to our text file storage
+            */
+        }
+    }
+}
---
+++ Law Clinic Skeleton/GUI2/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EvaluateMeans_Click(object sender, RoutedEventArgs e)
+        {
+            /* 
+            Based on the figures entered, it can be determined whether or not the client in question qualifies for aid
+            One of three outcomes then may be dediced upon by the client
+            1. Client Qualifies and books appointment
+            2. Client doesn’t qualify but requires advice
+            3. Client doesn't qualify but doesn’t want advice
+            */
+        }
+
+        private void Appointment_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            We will need to design a way to manage and add appointments
+            This button should only be enabled if the means test as already been completed
+            */
+        }
+
+        private void Meeting_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            Same as the Appointment Button
+            What's the difference between a meeting and an appointment
+            */
+        }
+    }
+
+}
